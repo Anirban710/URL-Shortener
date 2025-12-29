7084ec9ca8f71c0b92ff3bef2126a7036f10004c
Anirban Bhattacharyya <arghab96@gmail.com>
2025-12-29 15:23:48 +0530

    Use cloud Redis credentials via environment variables

--- config/Redis.js
+++ config/Redis.js
@@ -1,14 +1,30 @@
+// const redis = require("redis");
+
+// const client = redis.createClient({
+//     socket: {
+//         host: "127.0.0.1",
+//         port: 6380   // we are using port 6380
+//     }
+// });
+
+// client.connect()
+//     .then(() => console.log("Redis connected"))
+//     .catch(err => console.error("Redis error", err));
+
+// module.exports = client;
+
 const redis = require("redis");
 
 const client = redis.createClient({
-    socket: {
-        host: "127.0.0.1",
-        port: 6380   // we are using port 6380
-    }
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
+  password: process.env.REDIS_PASSWORD
 });
 
 client.connect()
-    .then(() => console.log("Redis connected"))
-    .catch(err => console.error("Redis error", err));
+  .then(() => console.log("Redis connected"))
+  .catch(err => console.error("Redis error", err));
 
 module.exports = client;
